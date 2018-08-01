883eff5c5c01a8b17e049e0a5203b5197f29e034
dylansd <dylansd@gmail.com>
2018-08-01 08:15:38 -0700

    Getting logic in shape

--- src/main/java/unified/service/DaylightService.java
+++ src/main/java/unified/service/DaylightService.java
@@ -2,6 +2,8 @@ package unified.service;
 
 import com.luckycatlabs.sunrisesunset.SunriseSunsetCalculator;
 import com.luckycatlabs.sunrisesunset.dto.Location;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.*;
 import java.util.Calendar;
@@ -11,6 +13,7 @@ import java.util.Calendar;
  */
 public class DaylightService {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
     private Location location = new Location("37.887509", "-122.54607");
     private SunriseSunsetCalculator calculator = new SunriseSunsetCalculator(location, "America/New_York");
 
@@ -26,15 +29,22 @@ public class DaylightService {
         LocalTime sunrise = LocalDateTime.ofInstant(officialSunrise.toInstant(), ZoneId.systemDefault()).toLocalTime();
         LocalTime sunset = LocalDateTime.ofInstant(officialSunset.toInstant(), ZoneId.systemDefault()).toLocalTime();
 
+        logger.info("Sunrise: " + sunrise);
+        logger.info("Sunset: " + sunset);
+
         if (now.getHour() >= 22) {
+            logger.info("Hour is greater or equal to 22, light should be off");
             return false;
         }
         if (now.getHour() <= 4) {
+            logger.info("Hour is less or equal to 4, light should be off");
             return false;
         }
         if (now.isAfter(sunrise.plusMinutes(30)) && now.isBefore(sunset.minusMinutes(30))) {
+            logger.info("Hour is in the middle of the day, light should be off");
             return false;
         }
+        logger.info("Reporting that light is on for time: " + now);
         return true;
     }
 
