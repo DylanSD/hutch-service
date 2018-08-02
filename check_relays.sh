670dce4217711fd455558cdb4aa3389fab4d7100
Dylan <dylansd@gmail.com>
2018-08-02 08:26:17 -0700

    check fixed

--- check_relays.sh
+++ check_relays.sh
@@ -3,13 +3,15 @@
 while (true) 
 do
         sleep 3m
+	date
         ANS=`curl -s -X GET http://localhost:8007/shouldLightBeOn`
 
         if [ "$ANS" == "true" ]
         then
-            curl -s -X PUT http://localhost:8007/relays/2/ON
+	    curl -s -X PUT http://localhost:8007/relays/2/ON
         else
             curl -s -X PUT http://localhost:8007/relays/2/OFF
         fi
+	echo 
 done
 
